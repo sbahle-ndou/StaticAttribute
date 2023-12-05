36100ac099a83126cdff7c35cd6bebcb6aa5a3d9
SBICZA01\A253563 <sbahle.ndou@standardbank.co.za>
2023-12-05 15:12:41 +0200

    Add project files.

---
+++ StaticAttribute/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAttribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs holiday = new Songs("Holiday", "Green Day");
+            Songs kashmir = new Songs("Kashmir", "Led Zeppelin");
+
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticAttribute/Songs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAttribute
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+
+        public static int songCount = 0;
+        public Songs(string aTitle, string aArtist, int aDuration);
+        {
+        this.title = aTitle;
+        this.artist = aArtist;
+        this.duration = aDuration;
+        songCount++;
+        }
+    }
+
